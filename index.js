6d9e5cc6f984174212e3a7e0ddb97eff737803e7
skepticfx <ahamed.nafeez@gmail.com>
2014-09-10 11:55:46 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tls = require('tls');
 var fs = require('fs');
+var debug = require('debug');
 
 var options = {
   key: fs.readFileSync(__dirname+'/certs/tlsecho.com.key'),
@@ -18,15 +19,11 @@ exports.start = function(port){
   var server = tls.createServer(options, function(cleartextStream) {
     cleartextStream.setEncoding('utf8');
     cleartextStream.pipe(cleartextStream);
-    cleartextStream.on('data', function(data){
-      console.log(data.toString())
-    })
-
   });
 
   port = port || 8000;
   server.listen(port, function() {
-    console.log('tls echo server started on port '+ port);
+    debug('tls echo server started on port '+ port);
   });
 
 return server;
